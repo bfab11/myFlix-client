58da18dc53c67d69f653c5cb5030acc77e5f2366
bfab11 <79426655+bfab11@users.noreply.github.com>
2021-07-02 17:47:49 -0700

    3.6 updated login view functionality

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,34 +2,44 @@ import React, { useState } from 'react';
 import './login-view.scss';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log(username, password);
         // Send a request to the server for authentication
-        // Then call props.onLoggedIn(username)
-        props.onLoggedIn(username);
+        axios.post('https://myflixdbapp.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
+        .then(response => {
+            const data = response.data;
+            props.onLoggedIn(data);
+        })
+        .catch(e => {
+            console.log('no such user')
+        });
     };
 
     return (
-        <form className="login-form">
-            <label className="loginput">
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label><br />
-            <label className="loginput">
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </label><br />
-            <span>
-                <Button variant="outline-dark" type="submit" onClick={handleSubmit}>Submit</Button>
-                <Button variant="outline-dark" type="secondary" onClick={props.toggleRegister}>Register</Button>
-            </span>
-        </form>
+        <Form>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="outline-dark" type="submit" onClick={handleSubmit}>Submit</Button>
+            <Button variant="outline-dark" type="secondary" onClick={props.toggleRegister}>Register</Button>
+        </Form>
     );
 }
 
